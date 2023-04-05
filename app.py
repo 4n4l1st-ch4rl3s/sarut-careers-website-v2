6b8b44740179356db3fb42f91e70beecaac350ec
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-06 02:43:15 +0300

    added new features

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('home.html')
+
+JOBS = [
+    {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Mombasa, Kenya',
+    'salary': '$90,000'
+    },
+    {
+    'id': 2,
+    'title': 'Data Scientists',
+    'location': 'Kisumu, Kenya',
+    'salary': '$75,000'
+    },
+    {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+    },
+    {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, California',
+    'salary': '$120,000'
+    },
+]
+
+@app.route('/')
+def hello_sarut():
+    return render_template('home.html',
+                           jobs=JOBS,
+                           company_name='Sarut')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
