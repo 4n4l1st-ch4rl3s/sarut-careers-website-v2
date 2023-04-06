eecbfc87996b9ce2115e1ec558efa7af8159d977
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-06 03:10:26 +0300

    more updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify,render_template
 
 app = Flask(__name__)
 
@@ -34,6 +34,10 @@ def hello_sarut():
     return render_template('home.html',
                            jobs=JOBS,
                            company_name='Sarut')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
